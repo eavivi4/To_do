8318c1d1c1a6360f5e4209a304a27abecd547b56
eavivi4 <avivi.eden@gmail.com>
2024-03-03 20:31:09 -0800

    Changed classes to ids and adding tasks to list is working

---
+++ index.js
@@ -0,0 +1,12 @@
+let addButton = document.getElementById('add');
+let container = document.getElementById('to_dos');
+let user_input = document.getElementById('user_in');
+
+addButton.addEventListener('click', function() {
+    // Make a paragraph element
+    var paragraph = document.createElement('p');
+    // Change the inner text of this paragraph to the value entered by the user
+    paragraph.innerText = user_input.value;
+    // Add this element into the container for the to do list
+    container.appendChild(paragraph);
+})
