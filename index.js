57b845571f02de4a854a999cf5d932043ccb8ad6
eavivi4 <avivi.eden@gmail.com>
2024-03-06 20:40:57 -0800

    Added button next to each item on list and option to color it

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ let container = document.getElementById('to_dos');
 let user_input = document.getElementById('user_in');
 
 addButton.addEventListener('click', function() {
-    // Make a paragraph element
+    // Make a paragraph element, div, and button elements
+    var div = document.createElement('div');
     var paragraph = document.createElement('p');
+    var check_button = document.createElement('button');
     // Change the inner text of this paragraph to the value entered by the user
     paragraph.innerText = user_input.value;
-    // Add this element into the container for the to do list
-    container.appendChild(paragraph);
+    
+    // Set class for button and div
+    check_button.className = 'check_button';
+    div.className = 'element';
+
+    // Add both paragraph element and button to the div for the item on list
+    container.appendChild(div);
+    div.appendChild(check_button);
+    div.appendChild(paragraph);
     // Empty input
     user_input.value = "";
 
@@ -19,7 +28,17 @@ addButton.addEventListener('click', function() {
 
     // Double clicking an element, remove
     paragraph.addEventListener('dblclick', function() {
-        container.removeChild(paragraph);
+        container.removeChild(div);
+    })
+
+    // Clicking on check button, will fill it with color
+    check_button.addEventListener('click', function() {
+        check_button.id = 'checked';
+    })
+
+    // Double clicking check button will uncolor it
+    check_button.addEventListener('dblclick', function() {
+        check_button.removeAttribute('id');
     })
 })
 
